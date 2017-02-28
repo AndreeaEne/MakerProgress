073ca1d96f60e8646e669e92466fcfd99ff126b9
Andreea-Daniela Ene <andreea.ene@devpadawan.ro>
2017-02-28 20:42:20 +0200

    image added

---
+++ MakerProgress/Controller/ViewController/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MakerProgress
+//
+//  Created by Andreea-Daniela Ene on 23/02/2017.
+//  Copyright © 2017 Andreea-Daniela Ene. All rights reserved.
+//	use synx
+//				/Users/andreea/Documents/iOS/DevAcademy/Project/MakerProgress/MakerProgress.xcodeproj
+//	to update
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+@end
